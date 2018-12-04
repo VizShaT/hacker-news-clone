2a5ee6c479641261a73cb5d8df75bdf5d9dff56a
kikit <kikit@users.noreply.github.com>
2018-12-04 18:07:29 +0530

    Redux store added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import ReactDOM from "react-dom";
 import App from "components/App";
 import setGlobalStyles from "styles/globals";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import configureStore from "store";
+
+const renderApp = () => {
+  const initialState = {};
+  const store = configureStore(initialState);
+
+  store.dispatch({ type: "@hnReader/@@INIT" });
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
 
 setGlobalStyles();
-ReactDOM.render(<App />, document.getElementById("root"));
-
+renderApp();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
